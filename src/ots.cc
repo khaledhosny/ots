khaledhosny/ots

commit 9f5b6e2e6a5c0182e4c89233aff8a4f6414d55e0

Make it possible to passthru required tables
Not entirely sure if it is a good idea, but we can revert it if we think
it is not.

Related to #15

src/ots.cc
@@ -572,8 +572,9 @@ bool ProcessGeneric(ots::OpenTypeFile *header, uint32_t signature,
     const std::map<uint32_t, OpenTypeTable>::const_iterator it
         = table_map.find(Tag(table_parsers[i].tag));
 
+    ots::TableAction action = GetTableAction(it->first);
     if (it == table_map.end()) {
-      if (table_parsers[i].required) {
+      if (table_parsers[i].required && action == ots::TABLE_ACTION_SANITIZE) {
         return OTS_FAILURE_MSG_TAG("missing required table", table_parsers[i].tag);
       }
       continue;
@@ -586,7 +587,6 @@ bool ProcessGeneric(ots::OpenTypeFile *header, uint32_t signature,
       return OTS_FAILURE_MSG_TAG("uncompress failed", table_parsers[i].tag);
     }
 
-    ots::TableAction action = GetTableAction(it->first);
     if (action == ots::TABLE_ACTION_SANITIZE &&
         !table_parsers[i].parse(header, table_data, table_length)) {
       // TODO: parsers should generate specific messages detailing the failure;

commit a9a5f453467d1494ebf9f31cba99c2c49b37fd52

Fix crash
It makes no sense to access “it->first” if “it == table_map.end()”, a
thinko introduced in 9f5b6e2e6a5c0182e4c89233aff8a4f6414d55e0.

src/ots.cc
@@ -573,7 +573,7 @@ bool ProcessGeneric(ots::OpenTypeFile *header, uint32_t signature,
     const std::map<uint32_t, OpenTypeTable>::const_iterator it
         = table_map.find(Tag(table_parsers[i].tag));
 
-    ots::TableAction action = GetTableAction(header, it->first);
+    ots::TableAction action = GetTableAction(header, Tag(table_parsers[i].tag));
     if (it == table_map.end()) {
       if (table_parsers[i].required && action == ots::TABLE_ACTION_SANITIZE) {
         return OTS_FAILURE_MSG_TAG("missing required table", table_parsers[i].tag);