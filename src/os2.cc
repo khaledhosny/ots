khaledhosny/ots

commit 0768b814162812c40f661eb57cb58b3a1835209d

[ots] Support OS/2 version 5
Part of #55.

src/os2.cc
@@ -39,7 +39,7 @@ bool ots_os2_parse(OpenTypeFile *file, const uint8_t *data, size_t length) {
     return OTS_FAILURE_MSG("Error reading basic table elements");
   }
 
-  if (os2->version > 4) {
+  if (os2->version > 5) {
     return OTS_FAILURE_MSG("Unsupported table version: %u", os2->version);
   }
 
@@ -156,7 +156,7 @@ bool ots_os2_parse(OpenTypeFile *file, const uint8_t *data, size_t length) {
   if ((os2->version < 4) &&
       (os2->selection & 0x300)) {
     // bit 8 and 9 must be unset in OS/2 table versions less than 4.
-    return OTS_FAILURE_MSG("OS2 version %d incompatible with selection %d", os2->version, os2->selection);
+    return OTS_FAILURE_MSG("Version %d incompatible with selection %d", os2->version, os2->selection);
   }
 
   // mask reserved bits. use only 0..9 bits.
@@ -206,7 +206,7 @@ bool ots_os2_parse(OpenTypeFile *file, const uint8_t *data, size_t length) {
       !table.ReadU16(&os2->default_char) ||
       !table.ReadU16(&os2->break_char) ||
       !table.ReadU16(&os2->max_context)) {
-    return OTS_FAILURE_MSG("Failed to read os2 version 2 information");
+    return OTS_FAILURE_MSG("Failed to read version 2-specific fields");
   }
 
   if (os2->x_height < 0) {
@@ -216,6 +216,26 @@ bool ots_os2_parse(OpenTypeFile *file, const uint8_t *data, size_t length) {
   if (os2->cap_height < 0) {
     OTS_WARNING("bad cap_height: %d", os2->cap_height);
     os2->cap_height = 0;
+  }
+
+  if (os2->version < 5) {
+    // http://www.microsoft.com/typography/otspec/os2ver4.htm
+    return true;
+  }
+
+  if (!table.ReadU16(&os2->lower_optical_pointsize) ||
+      !table.ReadU16(&os2->upper_optical_pointsize)) {
+    return OTS_FAILURE_MSG("Failed to read version 5-specific fields");
+  }
+
+  if (os2->lower_optical_pointsize > 0xFFFE) {
+    OTS_WARNING("'usLowerOpticalPointSize' is bigger than 0xFFFE: %d", os2->lower_optical_pointsize);
+    os2->lower_optical_pointsize = 0xFFFE;
+  }
+
+  if (os2->upper_optical_pointsize < 2) {
+    OTS_WARNING("'usUpperOpticalPointSize' is lower than 2: %d", os2->upper_optical_pointsize);
+    os2->upper_optical_pointsize = 2;
   }
 
   return true;
@@ -266,7 +286,7 @@ bool ots_os2_serialise(OTSStream *out, OpenTypeFile *file) {
       !out->WriteS16(os2->typo_linegap) ||
       !out->WriteU16(os2->win_ascent) ||
       !out->WriteU16(os2->win_descent)) {
-    return OTS_FAILURE_MSG("Failed to write os2 version 1 information");
+    return OTS_FAILURE_MSG("Failed to write version 1-specific fields");
   }
 
   if (os2->version < 1) {
@@ -287,7 +307,16 @@ bool ots_os2_serialise(OTSStream *out, OpenTypeFile *file) {
       !out->WriteU16(os2->default_char) ||
       !out->WriteU16(os2->break_char) ||
       !out->WriteU16(os2->max_context)) {
-    return OTS_FAILURE_MSG("Failed to write os2 version 2 information");
+    return OTS_FAILURE_MSG("Failed to write version 2-specific fields");
+  }
+
+  if (os2->version < 2) {
+    return true;
+  }
+
+  if (!out->WriteU16(os2->lower_optical_pointsize) ||
+      !out->WriteU16(os2->upper_optical_pointsize)) {
+    return OTS_FAILURE_MSG("Failed to write version 5-specific fields");
   }
 
   return true;

src/os2.h
@@ -47,6 +47,8 @@ struct OpenTypeOS2 {
   uint16_t default_char;
   uint16_t break_char;
   uint16_t max_context;
+  uint16_t lower_optical_pointsize;
+  uint16_t upper_optical_pointsize;
 };
 
 }  // namespace ots

commit b057e4d68ea39886903ba0015b583950e6176292

[os2] Fix use of uninitialized value
Thanks to Kunihiko Sakamoto.

A typo introduced in 0768b814162812c40f661eb57cb58b3a1835209d.

src/os2.cc
@@ -310,7 +310,7 @@ bool ots_os2_serialise(OTSStream *out, OpenTypeFile *file) {
     return OTS_FAILURE_MSG("Failed to write version 2-specific fields");
   }
 
-  if (os2->version < 2) {
+  if (os2->version < 5) {
     return true;
   }
 