e3b7c90c45d90d1c9217ce54394118bee54ad34d
Khaled Hosny <khaledhosny@eglug.org>
2015-04-11 15:51:47 +0200

    [build] Add autogen.sh for convenience

---
+++ autogen.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo -n "checking for git... "
+[ -d .git ] && which git || {
+	echo "*** No git found, please install it ***"
+	exit 1
+}
+
+echo "running git submodule init"
+git submodule init
+
+echo "running git submodule update"
+git submodule update
+
+echo -n "checking for pkg-config... "
+which pkg-config || {
+	echo "*** No pkg-config found, please install it ***"
+	exit 1
+}
+
+echo -n "checking for autoreconf... "
+which autoreconf || {
+	echo "*** No autoreconf (autoconf) found, please install it ***"
+	exit 1
+}
+
+echo "running autoreconf --force --install --verbose"
+autoreconf --force --install --verbose || exit $?
