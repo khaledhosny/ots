57c39e1ec7e30590821f5479d9964b6afb911bf4
Khaled Hosny <khaledhosny@eglug.org>
2015-05-29 14:28:10 +0200

    [collection] Add missing return statement
    The intent was to return on error here. Part of #65.

--- src/ots.cc
+++ src/ots.cc
@@ -280,7 +280,7 @@ bool ProcessTTC(ots::OpenTypeFile *header,
   uint32_t offsets[num_fonts];
   for (unsigned i = 0; i < num_fonts; i++) {
     if (!file.ReadU32(&offsets[i])) {
-      OTS_FAILURE_MSG_HDR("Error reading offset to OffsetTable");
+      return OTS_FAILURE_MSG_HDR("Error reading offset to OffsetTable");
     }
   }
 
