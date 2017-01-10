aaf5c4632ed17cd484f58711c2115446c159a739
Khaled Hosny <khaledhosny@eglug.org>
2017-01-10 23:55:56 +0200

    Dynamically allocating memory might be even better

--- util/ots-fuzzer.cc
+++ util/ots-fuzzer.cc
@@ -13,12 +13,10 @@
 #include "opentype-sanitiser.h"
 #include "ots-memory-stream.h"
 
-static uint8_t buffer[256 * 1024] = { 0 };
-
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   ots::OTSContext context;
-  ots::MemoryStream stream(static_cast<void*>(buffer), sizeof(buffer));
+  ots::ExpandingMemoryStream stream(size /*initial*/, size * 8 /*limit*/);
   context.Process(&stream, data, size);
   return 0;
 }
