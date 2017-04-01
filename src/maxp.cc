20ed94a7b93cda785936e367600afde63e8b5d5b
Khaled Hosny <khaledhosny@eglug.org>
2017-04-01 19:46:48 +0200

    [maxp] Improve error messages

--- src/maxp.cc
+++ src/maxp.cc
@@ -14,19 +14,19 @@ bool OpenTypeMAXP::Parse(const uint8_t *data, size_t length) {
 
   uint32_t version = 0;
   if (!table.ReadU32(&version)) {
-    return Error("Failed to read version of maxp table");
+    return Error("Failed to read table version");
   }
 
   if (version >> 16 > 1) {
-    return Error("Bad maxp version %d", version);
+    return Error("Unsupported table version 0x%x", version);
   }
 
   if (!table.ReadU16(&this->num_glyphs)) {
-    return Error("Failed to read number of glyphs from maxp table");
+    return Error("Failed to read numGlyphs");
   }
 
   if (!this->num_glyphs) {
-    return Error("Bad number of glyphs 0 in maxp table");
+    return Error("numGlyphs is 0");
   }
 
   if (version >> 16 == 1) {
@@ -44,21 +44,21 @@ bool OpenTypeMAXP::Parse(const uint8_t *data, size_t length) {
         !table.ReadU16(&this->max_size_glyf_instructions) ||
         !table.ReadU16(&this->max_c_components) ||
         !table.ReadU16(&this->max_c_depth)) {
-      return Error("Failed to read maxp table");
+      return Error("Failed to read version 1 table data");
     }
 
     if (this->max_zones == 0) {
       // workaround for ipa*.ttf Japanese fonts.
-      Warning("bad max_zones: %u", this->max_zones);
+      Warning("Bad maxZones: %u", this->max_zones);
       this->max_zones = 1;
     } else if (this->max_zones == 3) {
       // workaround for Ecolier-*.ttf fonts.
-      Warning("bad max_zones: %u", this->max_zones);
+      Warning("Bad maxZones: %u", this->max_zones);
       this->max_zones = 2;
     }
 
     if ((this->max_zones != 1) && (this->max_zones != 2)) {
-      return Error("Bad max zones %d in maxp", this->max_zones);
+      return Error("Bad maxZones: %u", this->max_zones);
     }
   } else {
     this->version_1 = false;
@@ -70,7 +70,7 @@ bool OpenTypeMAXP::Parse(const uint8_t *data, size_t length) {
 bool OpenTypeMAXP::Serialize(OTSStream *out) {
   if (!out->WriteU32(this->version_1 ? 0x00010000 : 0x00005000) ||
       !out->WriteU16(this->num_glyphs)) {
-    return Error("Failed to write maxp version or number of glyphs");
+    return Error("Failed to write version or numGlyphs");
   }
 
   if (!this->version_1) return true;
