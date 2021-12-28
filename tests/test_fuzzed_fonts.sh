3f0a0deefae7f2fe15b34f670b4c1faeef2ddfa9
Khaled Hosny <khaled@aliftype.com>
2021-12-28 05:04:56 +0200

    [test] Cleanup and normalize test scripts

--- tests/test_fuzzed_fonts.sh
+++ tests/test_fuzzed_fonts.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
-# Copyright (c) 2017 The OTS Authors. All rights reserved.
+# Copyright (c) 2017-2021 The OTS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-test "x$top_srcdir" = x && top_srcdir=.
-test "x$top_builddir" = x && top_builddir=.
+# Usage: ./test_fuzzed_fonts.sh ots-fuzzer fonts_dir [ttf_or_otf_file_name]
 
-# Usage: ./test_fuzzed_fonts.sh [ttf_or_otf_file_name]
-
-BASE_DIR=$top_srcdir/tests/fonts
-CHECKER=$top_builddir/ots-fuzzer$EXEEXT
+CHECKER=$1
+BASE_DIR=$2
 
 if [ ! -x "$CHECKER" ] ; then
   echo "$CHECKER is not found."
   exit 1
 fi
 
-if [ $# -eq 0 ] ; then
+if [ $# -eq 2 ] ; then
   # No font file is specified. Apply this script to all TT/OT files under the
   # BASE_DIR.
+  if [ ! -d "$BASE_DIR" ] ; then
+    echo "$BASE_DIR does not exist."
+    exit 1
+  fi
 
+  FONTS=$(find "$BASE_DIR" -type f)
   # Recursively call this script.
   FAILS=0
-  FONTS=$(find $BASE_DIR -type f -name '*tf' -o -name '*tc' -o -name '*woff*')
   IFS=$'\n'
   for f in $FONTS; do
-    $0 "$f"
+    $0 "$CHECKER" "$BASE_DIR" "$f"
     FAILS=$((FAILS+$?))
   done
 
@@ -39,17 +40,17 @@ if [ $# -eq 0 ] ; then
   fi
 fi
 
-if [ $# -gt 1 ] ; then
-  echo "Usage: $0 [ttf_or_otf_file_name]"
+if [ $# -gt 3 ] ; then
+  echo "Usage: $0 ots-fuzzer fonts_dir [ttf_or_otf_file_name]"
   exit 1
 fi
 
 # Check the font file using ots-fuzzer.
-$CHECKER "$1" 2>&1
-RET=$?
-if [ $RET != 0 ]; then
+$CHECKER "$3" 2>&1
+if [ $? != 0 ]; then
   echo "FAILED: $1"
+  exit 1
 else
   echo "PASSED: $1"
+  exit 0
 fi
-exit $RET
--- tests/test_good_fonts.sh
+++ tests/test_good_fonts.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-# Copyright (c) 2009-2017 The OTS Authors. All rights reserved.
+# Copyright (c) 2009-2021 The OTS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-test "x$top_srcdir" = x && top_srcdir=.
-test "x$top_builddir" = x && top_builddir=.
+# Usage: ./test_bad_fonts.sh ots-idempotent fonts_dir [ttf_or_otf_file_name]
 
-# Usage: ./test_good_fonts.sh [ttf_or_otf_file_name]
-
-BASE_DIR=$top_srcdir/tests/fonts/good/
-CHECKER=$top_builddir/ots-idempotent$EXEEXT
+CHECKER=$1
+BASE_DIR=$2
 
 if [ ! -x "$CHECKER" ] ; then
   echo "$CHECKER is not found."
   exit 1
 fi
 
-if [ $# -eq 0 ] ; then
+if [ $# -eq 2 ] ; then
   # No font file is specified. Apply this script to all TT/OT files under the
   # BASE_DIR.
   if [ ! -d "$BASE_DIR" ] ; then
@@ -25,12 +22,12 @@ if [ $# -eq 0 ] ; then
     exit 1
   fi
 
-  FONTS=$(find $BASE_DIR -type f)
+  FONTS=$(find "$BASE_DIR" -type f)
   # Recursively call this script.
   FAILS=0
   IFS=$'\n'
   for f in $FONTS; do
-    $0 "$f"
+    $0 "$CHECKER" "$BASE_DIR" "$f"
     FAILS=$((FAILS+$?))
   done
 
@@ -43,13 +40,13 @@ if [ $# -eq 0 ] ; then
   fi
 fi
 
-if [ $# -gt 1 ] ; then
-  echo "Usage: $0 [ttf_or_otf_file_name]"
+if [ $# -gt 3 ] ; then
+  echo "Usage: $0 ots-idempotent fonts_dir [ttf_or_otf_file_name]"
   exit 1
 fi
 
 # Confirm that the good font file is accepted by OTS.
-$CHECKER "$1" 2>&1
+$CHECKER "$3" 2>&1
 if [ $? != 0 ]; then
   echo "FAILED: $1"
   exit 1
