9f7c4b1f9dee0851a30e1af745ad9542f65f4a7b
Hans-Filip Elo <hans-filip.elo@xperi.com>
2025-05-23 09:26:41 +0200

    New overload OTSContext::Process to lower memory use processing WOFF2
    NOTE: This patch is intrusive. It's not necessarily expected that this
    patch can land in its current state, rather it is provided to spur
    discussion.
    
    We are Chromium integrators and we've seen that processing some WOFF2
    fonts can take up a lot of memory. This patch adds a new overload to
    OTSContext::Process in order to make it possible to use less memory
    while processing WOFF2 fonts. It does so via the following changes:
    
    - Makes ots::ExpandingMemoryStream part of public ots API.
    - Makes ots::ExpandingMemoryStream handle move-construct and
      move-assignment.
    - Adds an overload of ots::OTSContext::Process that takes an rvalue ref
      to ots::ExpandingMemoryStream as input buffer.
    - Makes ots::OTSContext::Process return the output buffer instead of
      writing to a separate output buffer.

--- include/ots-memory-stream.h
+++ include/ots-memory-stream.h
@@ -47,63 +47,6 @@ class MemoryStream : public OTSStream {
   off_t off_;
 };
 
-class ExpandingMemoryStream : public OTSStream {
- public:
-  ExpandingMemoryStream(size_t initial, size_t limit)
-      : length_(initial), limit_(limit), off_(0) {
-    ptr_ = new uint8_t[length_];
-  }
-
-  ~ExpandingMemoryStream() {
-    delete[] static_cast<uint8_t*>(ptr_);
-  }
-
-  void* get() const {
-    return ptr_;
-  }
-
-  size_t size() override { return limit_; }
-
-  bool WriteRaw(const void *data, size_t length) override {
-    if ((off_ + length > length_) ||
-        (length > std::numeric_limits<size_t>::max() - off_)) {
-      if (length_ == limit_)
-        return false;
-      size_t new_length = (length_ + 1) * 2;
-      if (new_length < length_)
-        return false;
-      if (new_length > limit_)
-        new_length = limit_;
-      uint8_t* new_buf = new uint8_t[new_length];
-      std::memcpy(new_buf, ptr_, length_);
-      length_ = new_length;
-      delete[] static_cast<uint8_t*>(ptr_);
-      ptr_ = new_buf;
-      return WriteRaw(data, length);
-    }
-    std::memcpy(static_cast<char*>(ptr_) + off_, data, length);
-    off_ += static_cast<off_t>(length);
-    return true;
-  }
-
-  bool Seek(off_t position) override {
-    if (position < 0) return false;
-    if (static_cast<size_t>(position) > length_) return false;
-    off_ = position;
-    return true;
-  }
-
-  off_t Tell() const override {
-    return off_;
-  }
-
- private:
-  void* ptr_;
-  size_t length_;
-  const size_t limit_;
-  off_t off_;
-};
-
 }  // namespace ots
 
 #endif  // OTS_MEMORY_STREAM_H_
